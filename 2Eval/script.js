65634adfe4583df64650b74793e30ae288a832c8
javipalacioss <painjavi@gmail.com>
2025-03-26 21:08:30 +0100

    JS

---
+++ 2Eval/script.js
@@ -0,0 +1,46 @@
+//cargar contenido desde el dom con eventListener
+document.addEventListener("DOMContentLoaded", () => {
+
+    // referencia al campo de entrada donde el usuario escribe la tarea
+    const inputTarea = document.getElementById("task-input");
+    //referencia al botón que agrega una nueva tarea
+    const botonAnadirTarea = document.getElementById("add-task-btn");
+    //referencia al contenedor donde se mostrarán las tareas agregadas
+    const contenedorListaTareas = document.getElementById("task-list-container");
+
+    //recupera la lista de tareas almacenadas en localStorage y la convierte de formato JSON a un array de objetos JavaScript
+    /*
+    || []: Si JSON.parse(...) devuelve null, se asigna un array vacío [] como valor predeterminado.
+    - Esto garantiza que tareas siempre sea un array, evitando errores al intentar manipularlo más adelante.
+    */
+    let tareas = (localStorage.getItem("tareas")) || [];
+
+    //añadir tarea
+    agregarTarea.addEventListener("click", () => {
+        const texto = taskInput.value.trim();
+
+        if (texto === "") return;
+
+        tareas.push({ text: texto, completed: false });
+        inputTarea.value = "";
+
+        //funcion para cargar tareas(completar)
+        cargarTareas();
+
+    });
+
+    //marcar como completada
+    function marcarCompletada(index) {
+        tareas[index].completed = !tareas[index].completed; 
+        cargarTareas();
+    }
+
+    //eliminar tarea
+    function eliminarTarea(index) {
+        tareas.splice(index, 1); //eliminar en la posicion index
+        renderizarTareas();
+    }
+
+    cargarTareas();
+
+});
